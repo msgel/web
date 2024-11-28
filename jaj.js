ce8a01c805c8d2d147f4f95a3e81f74ac8195a60
msgel <kitkat0jali@gmail.com>
2024-11-28 21:32:06 +0800

    Initial commit

---
+++ jaj.js
@@ -0,0 +1,11 @@
+document.getElementById('album1').addEventListener('click', function() {
+    window.location.href = 'cutterpillow.html';
+});
+
+document.getElementById('album2').addEventListener('click', function() {
+    window.location.href = 'buwan_ng_walang_hanggan.html';
+});
+
+document.getElementById('album3').addEventListener('click', function() {
+    window.location.href = 'sticker_happy.html';
+});
